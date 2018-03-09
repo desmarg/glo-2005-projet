1235e0960afb40e907e344f70a450ef360f35faa
Guillaume D De Grandpre <guillaume.d.degrandpre@gmail.com>
2018-03-09 14:19:08 -0500

    Add React Router skeleton

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 export default class App extends React.Component {
-  render () {
-    return <p> Hello React!</p>;
+  render() {
+    return (
+      <div>
+        <Header />
+        <Main />
+      </div>
+    )
   }
 }
--- static/js/index.jsx
+++ static/js/index.jsx
@@ -1,4 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
-ReactDOM.render(<App />, document.getElementById("content"));
+ReactDOM.render((
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+), document.getElementById("content"));
