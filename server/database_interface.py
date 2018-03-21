e13baf5f6a3e38add2c31ac2fcdf92b2db457b69
Guillaume D De Grandpre <guillaume.d.degrandpre@gmail.com>
2018-03-21 14:27:19 -0400

    Database connection is now in utf8

--- server/database_interface.py
+++ server/database_interface.py
@@ -5,10 +5,23 @@ import pymysql.cursors
 # Read MySQL config info from config/database.ini
 dbConfig = configparser.ConfigParser()
 dbConfig.read('config/database.ini')
-MySQLConfig = dbConfig['MYSQL']
-host, user, password, db = MySQLConfig['host'], MySQLConfig['user'], MySQLConfig['password'], MySQLConfig['db']
+mySQLConfig = dbConfig['MYSQL']
+host, user, password, db = mySQLConfig['host'], mySQLConfig['user'], mySQLConfig['password'], mySQLConfig['db']
 
 # Initialize connection to the MySQL DB
-connection = pymysql.connect(host, user, password, db)
+connection = pymysql.connect(host, user, password, db, charset="utf8")
 cursor = connection.cursor()
 
+class RecipeDAO:
+    def __init__(self):
+        self.cursor = cursor
+    
+    def searchByIngredients(self, ingredientArray):
+        pass
+
+class UserDAO:
+    def __init__(self):
+        self.cursor = cursor
+
+    def createUser(self):
+        pass    
