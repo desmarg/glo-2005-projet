0292c9a8a11b14d6febab02d1b4fafec1b47fc26
Guillaume D De Grandpre <guillaume.d.degrandpre@gmail.com>
2018-03-27 13:21:01 -0400

    Implement basic search on ingredients page

--- static/js/components/pages/Ingredients.jsx
+++ static/js/components/pages/Ingredients.jsx
@@ -19,8 +19,30 @@ export default class Ingredients extends React.Component {
                     return;
                 }
 
-                response.json().then(function(data) {
-                    console.log(data);
+                response.json().then(data => {
+                    this.setState({
+                        ingredients: data
+                    })
+                });
+            }
+        )
+        .catch(err => {
+            console.log('Fetch Error :-S', err);
+        });
+    }
+
+    onSearchChange(e) {
+        fetch(Config.serverUrl + '/ingredients?search=' + e.target.value)
+        .then(response => {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' + response.status);
+                    return;
+                }
+
+                response.json().then(data => {
+                    this.setState({
+                        ingredients: data
+                    })
                 });
             }
         )
@@ -31,7 +53,14 @@ export default class Ingredients extends React.Component {
 
     render() {
         return (
-            <div> </div>
+            <div>
+                <input type="text" onChange={e => this.onSearchChange(e)}/>
+                {
+                    this.state.ingredients.map(ingredient => (
+                        <div key={ingredient.id}> {ingredient.name} </div>
+                    ))
+                }
+            </div>
         )
     }
 }
