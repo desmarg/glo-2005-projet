a80f7afe13654965da9cb3ce055c75984674f35c
marcoandre1 <31892221+marcoandre1@users.noreply.github.com>
2018-03-27 16:26:23 -0400

    remove checks in VoteDAO update and remove

--- server/VoteDAO.py
+++ server/VoteDAO.py
@@ -12,20 +12,12 @@ class VoteDAO:
         self.cursor.execute(request, (recipe, email, newVote))
 
     def update(self, recipe, email, editVote):
-        request = "SELECT COUNT(*) FROM Votes WHERE email = %s AND recipe = %s"
-        self.cursor.execute(request, (email, recipe))
-        ratingCount = self.cursor.fetchone()[0]
-        if ratingCount > 0:
-            request = "UPDATE Votes SET rating = %s WHERE recipe = %s AND email = %s"
-            self.cursor.execute(request, (editVote, recipe, email))
+        request = "UPDATE Votes SET rating = %s WHERE recipe = %s AND email = %s"
+        self.cursor.execute(request, (editVote, recipe, email))
 
     def remove(self, recipe, email):
-        request = "SELECT COUNT(*) FROM Votes WHERE email = %s AND recipe = %s"
-        self.cursor.execute(request, (email, recipe))
-        ratingCount = self.cursor.fetchone()[0]
-        if ratingCount > 0:
-            request = "DELETE FROM Votes WHERE recipe = %s AND email = %s"
-            self.cursor.execute(request, (recipe, email))
+        request = "DELETE FROM Votes WHERE recipe = %s AND email = %s"
+        self.cursor.execute(request, (recipe, email))
 
     def getFromUser(self, email):
         request = "SELECT recipe, rating FROM Votes WHERE email = %s"
