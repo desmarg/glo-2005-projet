bbeb6fc4bfa62631c197faaf35f4868d976f38e2
marcoandre1 <31892221+marcoandre1@users.noreply.github.com>
2018-04-14 12:46:41 -0400

    Correction mineure sur Login "class" --> "className" & creation de la page profile.

--- static/js/components/pages/Login.jsx
+++ static/js/components/pages/Login.jsx
@@ -4,20 +4,20 @@ require('../../../css/login.css')
 export default class Login extends React.Component {
     render() {
         return (
-            <div class="login-page">
-                <div class="form">
-                    <form class="register-form">
+            <div className="login-page">
+                <div className="form">
+                    <form className="register-form">
                     <input type="text" placeholder="name"/>
                     <input type="password" placeholder="password"/>
                     <input type="text" placeholder="email address"/>
                     <button>create</button>
-                    <p class="message">Already registered? <a href="#">Sign In</a></p>
+                    <p className="message">Already registered? <a href="#">Sign In</a></p>
                     </form>
-                    <form class="login-form">
+                    <form className="login-form">
                     <input type="text" placeholder="username"/>
                     <input type="password" placeholder="password"/>
                     <button>login</button>
-                    <p class="message">Not registered? <a href="#">Create an account</a></p>
+                    <p className="message">Not registered? <a href="#">Create an account</a></p>
                     </form>
                 </div>
             </div>
--- static/js/components/pages/Profile.jsx
+++ static/js/components/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+require('../../../css/profile.css');
 
 const Config = require('Config');
 
@@ -23,7 +24,11 @@ export default class Profile extends React.Component {
                 }
 
                 response.json().then(data => {
-                    this.setState({comments: data})
+                    this.setState({
+                        comments: data.comments,
+                        votes : data.votes,
+                        favorites : data.favorites
+                    })
                 });
             }
         )
@@ -33,8 +38,79 @@ export default class Profile extends React.Component {
     }
 
     render() {
+        /*const {votes} = this.state;
+
+          // total number of stars
+        const starTotal = 5;
+          
+        for(const rating in votes) {  
+            const starPercentage = (votes[rating] / starTotal) * 100;
+            const starPercentageRounded = `${(Math.round(starPercentage / 10) * 10)}%`;
+            document.querySelector(`.${rating} .stars-inner`).style.width = starPercentageRounded; 
+        }*/
         return (
-            <div> </div>
-        )
+            <div>
+                {/*<!-- Comment section start -->*/}
+                <div className="comments">
+                <h3><span>Your comments on our vegan recipes</span></h3>
+                {
+                    this.state.comments.map((comment, index) => (
+                        <div key={index}> <h6>Comment on {comment.recipe}</h6> {comment.content}
+                        <p className="commenttime">{comment.commenttime}</p> </div>
+                    ))
+                }
+                </div>
+                {/*<!-- Comment section end -->*/}
+
+                {/*<!-- Vote section start -->*/}
+                <div className="votes">
+                <h3><span>Your voted vegan recipes</span></h3>
+                {
+                    this.state.votes.map((vote, index) => (
+                        <div key={index}> {vote.recipe}
+                        <div className="stars-outer">
+                            <div className="stars-inner"></div>
+                        </div>
+                        </div>
+                    ))
+                }
+
+                {/*
+                    this.state.votes.map((vote, index) => (
+                        <div key={index}> {vote.recipe} {vote.rating}
+                        <p className="rating">
+                        <input type="radio" name="rating" id="str5" value="5" class="radio-btn hide"/><label for="str5">☆</label>
+                        <input type="radio" name="rating" id="str4" value="4" class="radio-btn hide"/><label for="str4">☆</label>
+                        <input type="radio" name="rating" id="str3" value="3" class="radio-btn hide"/><label for="str3">☆</label>
+                        <input type="radio" name="rating" id="str2" value="2" class="radio-btn hide"/><label for="str2">☆</label>
+                        <input type="radio" name="rating" id="str1" value="1" class="radio-btn hide"/><label for="str1">☆</label>
+                        </p>
+                        </div>
+                    ))
+                */}
+                </div>
+                {/*<!-- Vote section end -->*/}
+{/*
+<div class="rating">
+    <input type="radio" name="rating" id="str5" value="5" class="radio-btn hide"/><label for="str5">☆</label>
+    <input type="radio" name="rating" id="str4" value="4" class="radio-btn hide"/><label for="str4">☆</label>
+    <input type="radio" name="rating" id="str3" value="3" class="radio-btn hide"/><label for="str3">☆</label>
+    <input type="radio" name="rating" id="str2" value="2" class="radio-btn hide"/><label for="str2">☆</label>
+    <input type="radio" name="rating" id="str1" value="1" class="radio-btn hide"/><label for="str1">☆</label>
+</div>
+*/}
+                {/*<!-- Favorite section start -->*/}
+                <div className="favorites">
+                <h3><span>Your favorite vegan recipes</span></h3>
+                {
+                    this.state.favorites.map((favorite, index) => (
+                        <div key={index}> {favorite.recipe} </div>
+                    ))
+                }
+                </div>
+                {/*<!-- Favorite section end -->*/}
+
+            </div>
+        ); 
     }
 }
